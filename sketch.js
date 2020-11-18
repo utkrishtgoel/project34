dcee9fc8b9096e6e430a908389bfe477f8349ccf
utkrishtgoel <70644293+utkrishtgoel@users.noreply.github.com>
2020-11-18 04:20:16 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+//Create variables here
+var dog,happyDog,database,foodS,foodStock
+var dogimg,dogimg1
+
+
+function preload()
+{
+  //load images here
+  
+ dogimg=loadImage("Dog.png")
+dogimg1=loadImage("happydog.png")
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(500,500);
+
+  dog=createSprite(250,300,150,150);
+  dog.addImage(dogimg);
+  dog.scale=0.15;
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(46,139,87);
+ 
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogimg1);
+  }
+
+  drawSprites();
+  fill(255,255,254);
+  stroke("black");
+  text("Food remaining : "+foodS,170,200);
+  textSize(13);
+  text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+}
+
+//Function to read values from DB
+function readStock(data){
+  foodS=data.val();
+}
+
+//Function to write values in DB
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  } 
+  database.ref('/').update({
+    Food:x
+  })
+}
